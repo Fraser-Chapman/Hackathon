a261653b28b64ec502857c53768bd5610b0fd771
alec.tunbridge <alec.tunbridge@autotrader.co.uk>
2019-02-08 12:09:09 +0000

    Last minute hacks to try get things working - mainly around JSON parsing

--- src/main/java/com/manchesterdigital/hackathon/domain/GridReference.java
+++ src/main/java/com/manchesterdigital/hackathon/domain/GridReference.java
@@ -1,10 +1,15 @@
 package com.manchesterdigital.hackathon.domain;
 
-public class GridReference {
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class gridReference {
+
+    @JsonProperty("gridRef")
     public String getGridRef() {
         return gridRef;
     }
 
+    @JsonProperty("gridRef")
     public void setGridRef(String gridRef) {
         this.gridRef = gridRef;
     }
